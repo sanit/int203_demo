8774de30ae58882ea8e25535c47cab4cf0eaa2fb
Sanit Sirisawatvatana <sanit@sit.kmutt.ac.th>
2021-03-03 11:35:49 +0700

    Add formdata object to store form data

---
+++ demo9/main7.js
@@ -0,0 +1,65 @@
+const constraints = {
+    firstname: {
+        presence: true,
+    },
+    lastname: {
+        presence: true,
+    },
+    age: {
+        presence: true,
+        numericality: {
+            lessThan: 150
+        }
+    },
+    gender: {
+        presence: true,
+    },
+    email: {
+        presence: true,
+        email: true
+    },
+    phone: {
+        presence: true,
+        length: {
+            minimum : 10,
+            message: "must be at least 10 digits"
+        },        
+    },
+    chosensubject: {
+        presence: true
+    }
+}
+
+var app =Vue.createApp({
+    data() {
+        return {
+            formdata: {
+                firstname: 'Sanit',
+                lastname: null,
+                age: null,
+                gender: null,
+                email: null,
+                phone: null,
+                chosensubject: null,
+            },
+            subject_lists: [{subject_id: 1, subject_name:'Intro to Vue 3'},
+                            {subject_id: 2, subject_name: 'Vue 3 Forms'},
+                            {subject_id: 3, subject_name: 'Vue 3 Reactivity'}],
+            errors: null,
+        }
+    },
+    methods: {
+        checkForm(){
+            console.log(this.formdata)
+            this.errors = validate(this.formdata,
+                                    constraints)
+            if(!this.errors){
+                alert("Registered successfully.")
+            }
+        }
+    }
+})
+
+app.mount('#app')
+
+
